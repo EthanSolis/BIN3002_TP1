015630ffa047c2033d45b4a151c22577aa1b188f
EthanSolis <67752606+EthanSolis@users.noreply.github.com>
2021-10-14 22:38:49 -0400

    Add files via upload

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 	
@@ -11,9 +13,11 @@ public class Main {
 		try {
 		File fasta = new File(path);
 		Scanner in = new Scanner(fasta);
-		
 		while(in.hasNextLine()) {
 			String line = in.nextLine();
+			if(line.isEmpty()) {
+				break;
+			}
 			if(line.charAt(0) == '>') {
 				if(!chrs.isEmpty()) {
 					chrs.add(currentChr);
@@ -24,6 +28,7 @@ public class Main {
 				currentChr += line;
 			}
 		}
+		chrs.add(currentChr);
 		in.close();
 		}
 		catch(FileNotFoundException e) {
@@ -32,4 +37,79 @@ public class Main {
 		
 		return chrs;
  	}
+	
+	
+	public static void main(String args[]) throws FileNotFoundException{
+		//args: fasta location, assumed number of genes, no. training iterations, output name
+		ArrayList<String> chromos = parse(args[0]);
+		
+		String chromo = chromos.get(0);
+		int iter = Integer.parseInt(args[2]);
+		
+		Model model = new Model(chromo, Integer.parseInt(args[1]));
+		
+		int[] segments;
+		do {
+			segments = model.segmentation(chromo);
+			model.apprentissage(segments, chromo);
+			iter--;
+		}
+		while(iter > 0);
+		segments = model.segmentation(chromo); //final segmentation
+		ArrayList<int[]> seqs = new ArrayList<>();
+		
+		//finds all the segments longer than 50 nucs
+		for(int i = 0; i<segments.length; i++) {
+			if(segments[i] == 1) {
+				for(int j = i; j<segments.length;  j++) {
+					if(segments[j] == 0) {
+						if(i-j >= 50) {
+							int[] seq = {i, j, i-j, 0}; // begin, end, length, gcContent
+							seqs.add(seq);
+						}
+						i = j;
+						break;
+					}
+				}
+			}
+		}
+		//calculate gc of each selected segment
+		for(int x = 0; x < seqs.size(); x++) {
+			int[] bounds = seqs.get(x);
+			int gcCount = 0;
+			for(int y = bounds[0]; y < bounds[1]; y++) {
+				if(chromo.charAt(y) == 'C' || chromo.charAt(y) == 'G') {
+					gcCount++;
+				}
+			}
+			bounds[3] = gcCount / bounds[2] * 1000; //gc score
+			seqs.set(x, bounds);
+		}
+		//creates output file
+	    try {
+	        File bed = new File(args[3]);
+	        if (bed.createNewFile()) {
+	          System.out.println("File created: " + bed.getName());
+	        } else {
+	          System.out.println("File already exists.");
+	        }
+	      } catch (IOException e) {
+	        System.out.println("Couldn't create the output file");
+	        e.printStackTrace();
+	      }
+	    //writes to output file
+	    try {
+	    	FileWriter output = new FileWriter(args[3]);
+	    	for(int i = 0; i < seqs.size(); i++) {
+	    		int[] gene = seqs.get(i);
+	    		output.write("chr1\t" + String.valueOf(gene[0]+1) + "\t" + String.valueOf(gene[1]+1) + "\t" + "gene"+String.valueOf(i) + "\t" + String.valueOf(gene[3]));
+	    	}
+	    	output.close();
+	    }
+	    catch(Exception e) {
+	        System.out.println("Couldn't write to output file");
+	        e.printStackTrace();
+	    }
+		
+	}
 }
--- TP1/src/Model.java
+++ TP1/src/Model.java
@@ -2,61 +2,140 @@ import java.lang.Math.*;
 
 
 public class Model {
-	private double p0;
-	private double p1;
-	private double trans0;
-	private double trans1;
-	// trans 0 = 0->1, trans 1 = 1->0
-	void getGC(String chr) {
-		float[] p = new float[2];
+	private double p0;			//p0 (probability of C or G in state 0)
+	private double p1;			//p1 (probability of C or G in state 1)
+	private double trans0;  	//transition 0 -> 1
+	private double trans1;  	//transition 1 -> 0
+
+	public Model (String chr, int genes) {
+		double[] p = new double[2];
+		int n = chr.length();
 		int count = 0;
-		for(int i=0; i<chr.length(); i++) {
+		for(int i=0; i<n; i++) {
 			if(chr.charAt(i)=='C' || chr.charAt(i)=='G') {
 				count++;
 			}
 		}
-		p[0] = count/chr.length();
+		p[0] = count/n;
 		p[1] = (p[0]+1)/2;
-		this.p0 = p[0];
-		this.p1 = p[1];
+
+		this.p0 = p[0]/2;
+		this.p1 = p[1]/2;
+		this.trans0 = genes/n;
+		this.trans1 = genes/(p[0]*n);
 	}
-	public double getp0() {
-		return this.p0;
-	}
-	
-	public void setp0(double val) {
-		this.p0 = val;
-	}
-	public double getp1() {
-		return this.p1;
-	}
-	
-	public void setp1(double val) {
-		this.p1 = val;
-	}
-	public double getTrans0() {
-		return this.trans0;
-	}
-	
-	public void setTrans0(double val) {
-		this.trans0 = val;
-	}
-	public double getTrans1() {
-		return this.trans1;
-	}
-	
-	public void setTrans1(double val) {
-		this.trans1 = val;
-	}
-	
+
+	//create segments meaning path of 0 and 1
+	//penalty = 1/2log(n) ; we are using BIC
 	int[] segmentation(String chr) {
 		int g = chr.length();
 		int[] segments = new int[g];
+		double[] v0 = new double[g];
+		double[] v1 = new double[g];
 		
+		//scores
+		double wAT = Math.log((1-p1)/(1-p0)) + Math.log((1-trans1)/(1-trans0));
+		double wCG = Math.log(p1/p0) + Math.log((1-trans1)/(1-trans0));
+		
+		//penalties
+		double penalty0 = Math.log((1-trans0)/trans1);
+		double penalty1 = Math.log((1-trans1)/trans0);
+	
+		v0[0] = 0;
 		for(int i = 0; i<g; i++) {
-			
+			if(i != 0) {
+				v0[i] = Math.max(v0[i-1],v1[i-1]-penalty0);
+			}
+			if(chr.charAt(i) == 'A' || chr.charAt(i) == 'T') {
+				if(i==0) {
+					v1[i] = wAT + (Math.log(trans0/trans1) - Math.log((1-trans1)/(1-trans0)));
+				}
+				else {
+					v1[i] = wAT + Math.max(v1[i-1], v0[i-1]-penalty1);
+				}
+			}
+			if(chr.charAt(i) == 'C' || chr.charAt(i) == 'G') {
+				if(i==0) {
+					v1[i] = wCG + (Math.log(trans0/trans1) - Math.log((1-trans1)/(1-trans0)));
+				}
+				else {
+					v1[i] = wCG + Math.max(v1[i-1], v0[i-1]-penalty1);
+				}
+			}
+			//smaller or bigger?
+			//do i have to put this out of the loop in order for it to work?
+			if(v0[i] > v1[i]) {
+				segments[i] = 0;
+			}
+			else {
+				segments[i] = 1;
+			}
 		}
 		
 		return segments;
+	}
+
+	//find new trans0 and trans1 from segments and sequence:
+	public void apprentissage (int[] segments, String sequence) {
+		int total0 = 0;
+		int totaltr0 = 0;
+		int tr01 = 0;
+		int total1 = 0;
+		int totaltr1 = 0;
+		int tr10 = 0;
+
+		int p0cg = 0;
+		int p1cg = 0;
+		double np0 = 0;
+		double np1 = 0;
+
+		//for transitions
+		for(int i=0; i<segments.length-1; i++) {
+			if(segments[i] == 0) {
+				if(segments[i+1] == 0) {
+					totaltr0 ++;
+				}
+				if(segments[i+1] == 1) {
+					totaltr0 ++;
+					tr01 ++;
+				}
+			}
+			if(segments[i] == 1) {
+				total1 ++;
+				if(segments[i+1] == 0) {
+					totaltr1 ++;
+					tr10 ++;
+				}
+				if(segments[i+1] == 1) {
+					totaltr1 ++;
+				}
+			}
+		}
+
+		//for p0 and p1
+		for(int k=0; k<segments.length; k++) {
+			if(segments[k] == 0) {
+				total0 ++;
+				if(sequence.charAt(k) == 'C' || sequence.charAt(k) == 'G') {
+					p0cg ++;
+				}
+			}
+			if(segments[k] == 1) {
+				total1 ++;
+				if(sequence.charAt(k) == 'C' || sequence.charAt(k) == 'G') {
+					p1cg ++;
+				}
+			}
+		}
+
+		np0 = p0cg/total0;
+		np1 = p1cg/total1;
+
+		this.p0 = np0/2;
+		this.p1 = np1/2;
+
+		this.trans0 = tr01/totaltr0;
+		this.trans1 = tr10/totaltr1;
+
 	}
 }
